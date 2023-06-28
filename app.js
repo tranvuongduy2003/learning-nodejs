96d2aeba6b020cbc0c4021e5aab460423b564600
Trần Vương Duy <136442876+tranvuongduy@users.noreply.github.com>
2023-06-28 15:29:09 +0700

    add 404 page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const bodyParser = require("body-parser");
+const path = require("path");
 const express = require("express");
 
 const adminRoutes = require("./routes/admin");
@@ -13,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,15 @@
-const expess = require("expess");
+const path = require("path");
+const express = require("express");
 
-const router = expess.Router();
+const router = express.Router();
 
 // /admin/add-product -> GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title" /><button type="submit"></button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // /admin/product -> POST
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
