1ceaebc9623144f49f3441b6e9643b5866cf2ec6
Trần Vương Duy <136442876+tranvuongduy@users.noreply.github.com>
2023-06-28 15:11:22 +0700

    use route

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const expess = require("expess");
+
+const router = expess.Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit"></button></form>'
+  );
+});
+
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
