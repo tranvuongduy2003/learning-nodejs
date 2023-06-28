d7da6cc1ff8d5d9b11e9865cd325f420578a6cec
Trần Vương Duy <136442876+tranvuongduy@users.noreply.github.com>
2023-06-28 15:16:24 +0700

    add views

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 // Nó mang ý nghĩa là một đối tượng body chứa dữ liệu mà đã được parsed sẽ được đưa vào request (có thể hiểu là req.body). Dữ liệu đó là một cặp key-value, trong đó value có thể là array hoặc string nếu extended: false và các loại còn lại nếu extended: true.
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const expess = require("expess");
 
 const router = expess.Router();
 
+// /admin/add-product -> GET
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit"></button></form>'
+    '<form action="/admin/product" method="POST"><input type="text" name="title" /><button type="submit"></button></form>'
   );
 });
 
+// /admin/product -> POST
 router.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
