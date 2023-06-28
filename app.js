511402ed19584dfebead6460ae9c51c42abb692a
Trần Vương Duy <136442876+tranvuongduy@users.noreply.github.com>
2023-06-28 15:00:29 +0700

    use body-parser

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const http = require("http");
-
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+// Nó mang ý nghĩa là một đối tượng body chứa dữ liệu mà đã được parsed sẽ được đưa vào request (có thể hiểu là req.body). Dữ liệu đó là một cặp key-value, trong đó value có thể là array hoặc string nếu extended: false và các loại còn lại nếu extended: true.
+
 app.use("/", (req, res, next) => {
-  console.log("This always run");
   next();
 });
 
 app.use("/products", (req, res, next) => {
-  console.log("This is Product Page");
   res.send("<h1>This is Product Page</h1>");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("Hello expess!");
   res.send("<h1>Hello expess!</h1>");
 });
 
